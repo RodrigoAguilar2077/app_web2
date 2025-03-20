fe4c1e590df0223177773b2ae4aa46e4b07c8e98
arcadia2014 <gonzalopezzcom@gmail.com>
2025-03-20 00:24:45 -0600

    version final2.3

--- integradora2/urls.py
+++ integradora2/urls.py
@@ -20,5 +20,6 @@ from pagina_web import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),  # Ruta para la página principal
+    path('', views.home, name="enviarcorreo"),  # Ruta para enviar el formulario
 ]
--- pagina_web/views.py
+++ pagina_web/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.core.mail import send_mail
 
 def home(request):
     return render(request, 'home.html')
+
+def enviarcorreo(request):
+    if request.method == "POST":
+        # Obtén los datos del formulario
+        subject = request.POST.get('asunto', '').encode('utf-8').decode('utf-8')  # Asegura codificación UTF-8
+        message = request.POST.get('mensaje', '').encode('utf-8').decode('utf-8') + \
+                  "\nRemitente: " + request.POST.get('correo', '').encode('utf-8').decode('utf-8')
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = ["autopetutt@gmail.com"]
+
+        # Enviar el correo
+        send_mail(subject, message, email_from, recipient_list)
+
+        # Redirigir a la sección 1 con un mensaje de éxito
+        return redirect('/#section1')
+    
+    return render(request, 'home.html')  # Asegúrate de que 'home.html' sea tu plantilla principal
